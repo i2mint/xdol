25bceef3a00db50bf6035f5978cbef91906f1455
Thor Whalen <1906276+thorwhalen@users.noreply.github.com>
2025-05-13 16:01:50 +0200

    feat: resolve_values_to_bytes

--- xdol/__init__.py
+++ xdol/__init__.py
@@ -3,3 +3,4 @@
 from xdol.pystores import PyFilesReader, PkgReader
 from xdol.util import resolve_module_filepath, resolve_to_folder
 from xdol.updating import update_with_policy
+from xdol.x_codecs import resolve_values_to_bytes
